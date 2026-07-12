774407bc42f2e19d95133114b7bdae5530fc9bb3
Mouhinf <mouhopap@gmail.com>
2026-07-12 12:19:31 +0000

    Add Bing Webmaster Tools verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -116,6 +116,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://res.cloudinary.com" />
         <link rel="dns-prefetch" href="https://res.cloudinary.com" />
         <meta name="google-site-verification" content="k3zie7KEq4iSgpYx6f6UfLjOQqqTc5jCHtY-t8LmPCs" />
+        <meta name="msvalidate.01" content="20EB0636974D89927504C9F070933B6F" />
         <script>
           {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","xla888qxl5");`}
         </script>
